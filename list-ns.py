d0b10aecd153715272e6cdc533a01f7895c22398
Arturo Fernandez <134515+bsnux@users.noreply.github.com>
2020-12-11 11:17:34 -0800

    Adding comments

--- list-ns.py
+++ list-ns.py
@@ -1,6 +1,8 @@
 import os
 from kubernetes import client, config
 
+
+# Reading configuration file and using current configuration
 config.load_kube_config(os.path.join(os.environ["HOME"], '.kube/config'))
 
 api = client.CoreV1Api()
